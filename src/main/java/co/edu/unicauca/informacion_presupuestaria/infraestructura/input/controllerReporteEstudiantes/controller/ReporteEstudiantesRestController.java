c6c526cc18cdad4d496f6b48e4cc9237d3e2b374
Alvaro Daniel Erazo Ramirez <alvaro.erazo@siigo.com>
2026-01-21 22:09:17 -0500

    Actualizar ReporteEstudiantesRestController: agregar mappers y mÃ©todos faltantes segÃºn diagrama de clases

--- src/main/java/co/edu/unicauca/informacion_presupuestaria/infraestructura/input/controllerReporteEstudiantes/controller/ReporteEstudiantesRestController.java
+++ src/main/java/co/edu/unicauca/informacion_presupuestaria/infraestructura/input/controllerReporteEstudiantes/controller/ReporteEstudiantesRestController.java
@@ -1,9 +1,22 @@
 package co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.controller;
 
 import co.edu.unicauca.informacion_presupuestaria.aplicacion.input.GestionarReporteEstudiantesCUIntPort;
+import co.edu.unicauca.informacion_presupuestaria.dominio.models.ConfiguracionReporteFinanciero;
 import co.edu.unicauca.informacion_presupuestaria.dominio.models.PeriodoAcademico;
+import co.edu.unicauca.informacion_presupuestaria.dominio.models.ProyeccionEstudiantes;
 import co.edu.unicauca.informacion_presupuestaria.dominio.models.ReporteEstudiantes;
 import co.edu.unicauca.informacion_presupuestaria.dominio.models.ReporteProyeccionEstudiantes;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.DTOAnswer.ConfiguracionReporteFinancieroDTORespuesta;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.DTOAnswer.ReporteEstudiantesDTORespuesta;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.DTOAnswer.ReporteProyeccionEstudiantesDTORespuesta;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.DTOPeticion.ConfiguracionReporteFinancieroDTOPeticion;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.DTOPeticion.PeriodoAcademicoDTOPeticion;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.DTOPeticion.ProyeccionEstudianteDTOPeticion;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.mappers.ConfiguracionReporteFinancieroMapperInfraestructura;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.mappers.PeriodoAcademicoMapperInfraestructura;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.mappers.ProyeccionEstudianteMapperInfraestructura;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.mappers.ReporteEstudiantesMapperInfraestructura;
+import co.edu.unicauca.informacion_presupuestaria.infraestructura.input.controllerReporteEstudiantes.mappers.ReporteProyeccionEstudiantesMapperInfraestructura;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,29 +27,88 @@ import org.springframework.web.bind.annotation.*;
 public class ReporteEstudiantesRestController {
     
     @Autowired
-    private GestionarReporteEstudiantesCUIntPort gestionarReporteEstudiantesCU;
+    private GestionarReporteEstudiantesCUIntPort objGestionarReporteEstudiantesCUInt;
+    
+    @Autowired
+    private ConfiguracionReporteFinancieroMapperInfraestructura objMapperConfiguracionReporteFinanciero;
+    
+    @Autowired
+    private ReporteProyeccionEstudiantesMapperInfraestructura objMapperReporteProyeccionEstudiantes;
+    
+    @Autowired
+    private ProyeccionEstudianteMapperInfraestructura objMapperProyeccionEstudiante;
+    
+    @Autowired
+    private PeriodoAcademicoMapperInfraestructura objMapperProyeccionPeriodoAcademico;
+    
+    @Autowired
+    private ReporteEstudiantesMapperInfraestructura objMapperReporteEstudiantes;
+
+    
+    @PutMapping("/configuracion-proyeccion")
+    public ResponseEntity<ConfiguracionReporteFinancieroDTORespuesta> actualizarConfiguracionProyeccion(
+            @RequestBody ConfiguracionReporteFinancieroDTOPeticion configuracion) {
+        try {
+            ConfiguracionReporteFinanciero configuracionDomain = 
+                objMapperConfiguracionReporteFinanciero.mappearDePeticionAConfiguracionReporteFinanciero(configuracion);
+            ConfiguracionReporteFinanciero configuracionActualizada = 
+                objGestionarReporteEstudiantesCUInt.actualizarConfiguracionProyeccion(configuracionDomain);
+            ConfiguracionReporteFinancieroDTORespuesta respuesta = 
+                objMapperConfiguracionReporteFinanciero.mappearDeConfiguracionReporteFinancieroARespuesta(configuracionActualizada);
+            return ResponseEntity.ok(respuesta);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/proyeccion-estudiante")
+    public ResponseEntity<ReporteProyeccionEstudiantesDTORespuesta> actualizarProyeccionEstudiante(
+            @RequestBody ProyeccionEstudianteDTOPeticion proyeccion) {
+        try {
+            ProyeccionEstudiantes proyeccionDomain = 
+                objMapperProyeccionEstudiante.mappearDePeticionAProyeccionEstudiante(proyeccion);
+            ReporteProyeccionEstudiantes reporte = 
+                objGestionarReporteEstudiantesCUInt.actualizarProyeccionEstudiante(proyeccionDomain);
+            ReporteProyeccionEstudiantesDTORespuesta respuesta = 
+                objMapperReporteProyeccionEstudiantes.mappearDeReporteProyeccionEstudiantesARespuesta(reporte);
+            return ResponseEntity.ok(respuesta);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+      
+    @PostMapping("/financiero")
+    public ResponseEntity<ReporteEstudiantesDTORespuesta> obtenerReporteFinanciero(
+            @RequestBody PeriodoAcademicoDTOPeticion periodo) {
+        try {
+            PeriodoAcademico periodoAcademico = 
+                objMapperProyeccionPeriodoAcademico.mappearDePeticionAPeriodoAcademico(periodo);
+            ReporteEstudiantes reporte = objGestionarReporteEstudiantesCUInt.obtenerReporteFinanciero(periodoAcademico);
+            ReporteEstudiantesDTORespuesta respuesta = 
+                objMapperReporteEstudiantes.mappearDeReporteEstudiantesARespuesta(reporte);
+            return ResponseEntity.ok(respuesta);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     
     @GetMapping("/proyeccion")
-    public ResponseEntity<ReporteProyeccionEstudiantes> obtenerProyeccionEstudiantes() {
+    public ResponseEntity<ReporteProyeccionEstudiantesDTORespuesta> obtenerProyeccionEstudiantes() {
         try {
-            ReporteProyeccionEstudiantes reporte = gestionarReporteEstudiantesCU.obtenerProyeccionEstudiantes();
-            return ResponseEntity.ok(reporte);
+            ReporteProyeccionEstudiantes reporte = objGestionarReporteEstudiantesCUInt.obtenerProyeccionEstudiantes();
+            ReporteProyeccionEstudiantesDTORespuesta respuesta = 
+                objMapperReporteProyeccionEstudiantes.mappearDeReporteProyeccionEstudiantesARespuesta(reporte);
+            return ResponseEntity.ok(respuesta);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
     
-    @GetMapping("/financiero")
-    public ResponseEntity<ReporteEstudiantes> obtenerReporteFinanciero(
-            @RequestParam Integer periodo,
-            @RequestParam Integer año) {
-        try {
-            PeriodoAcademico periodoAcademico = new PeriodoAcademico(periodo, año);
-            ReporteEstudiantes reporte = gestionarReporteEstudiantesCU.obtenerReporteFinanciero(periodoAcademico);
-            return ResponseEntity.ok(reporte);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
+
+    
+
+  
 }
 
